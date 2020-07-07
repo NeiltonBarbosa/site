02cd2a6b493f107411c3584a4a2cca35a4dc1bc8
Neilton Barbosa <neiltonsb.nb@gmail.com>
2020-07-06 22:30:41 -0400

    Primeiro Commit

---
+++ backend/src/shared/infra/typeorm/migrations/1594087014624-CreateCategories.ts
@@ -0,0 +1,9 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+
+export default class CreateCategories1594087014624
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {}
+}
